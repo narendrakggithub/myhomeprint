c483e049fb26f9ea7ecb39e655c68e453114c422
narendrakg <66894171+narendrakggithub@users.noreply.github.com>
2022-12-30 13:13:51 +0530

    print

--- src/main/java/com/narenkg/myhomeprint/modal/PrinterSetting.java
+++ src/main/java/com/narenkg/myhomeprint/modal/PrinterSetting.java
@@ -1,16 +1,10 @@
 package com.narenkg.myhomeprint.modal;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 
-@Entity
+
 public class PrinterSetting {
 	
-	@Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private long id;
     
 	private String pageNumbers;
