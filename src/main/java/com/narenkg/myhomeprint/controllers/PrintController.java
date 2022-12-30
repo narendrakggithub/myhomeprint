47b13eabc45e633afe50d8d8f6dba97404e12796
narendrakg <66894171+narendrakggithub@users.noreply.github.com>
2022-12-30 13:36:23 +0530

    print

--- src/main/java/com/narenkg/myhomeprint/controllers/PrintController.java
+++ src/main/java/com/narenkg/myhomeprint/controllers/PrintController.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.*;
 
 import com.narenkg.myhomeprint.modal.PrinterSetting;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 @RequestMapping("")
 @Controller
 public class PrintController {
+
+	ArrayList<PrinterSetting> printerSettings = new ArrayList<PrinterSetting>();
 
 	@GetMapping("/hello")
 	public String hello() {
@@ -18,21 +22,41 @@ public class PrintController {
 
 	@RequestMapping("/print")
 	public String greetingForm(Model model) {
-		model.addAttribute("printSetting", new PrinterSetting());
+		PrinterSetting settings = new PrinterSetting();
+		settings.setPageNumbers("1-2");
+		settings.setBothSide(true);
+		settings.setIsColor(false);
+		settings.setLandscape(true);
+		settings.setPagesPerSheet(1);
+		settings.setMessage("India@123");
+		model.addAttribute("printSetting", settings);
 		return "print";
 	}
 
-	@RequestMapping(value="/print", method=RequestMethod.POST)    
+	@RequestMapping(value = "/print", method = RequestMethod.POST)
 	public String greetingSubmit(@ModelAttribute PrinterSetting settings, Model model) {
 		
-		if(settings.getMessage().equals("India@123")) {
+
+		if (settings.getMessage().equals("India@123")) {
 			model.addAttribute("printSetting", settings);
+			printerSettings.add(settings);
+			System.out.println("Size:"+printerSettings.size());
 			return "result";
 		} else {
 			model.addAttribute("message", "Not a correct password");
 			return "error";
 		}
-		
-		
+	}
+
+	@GetMapping("/getprint")
+	public String getPrint(Model model) {
+		String output = "";
+		for (PrinterSetting setting : printerSettings) {
+			output = output + setting.getPageNumbers() + ";" + setting.getBothSide() + ";" + setting.getIsColor() + ";"
+					+ setting.getLandscape() + ";" + setting.getPagesPerSheet() + ";;;";
+		}
+		//printerSettings.clear();
+		model.addAttribute("message", output);
+		return "printpages";
 	}
 }
